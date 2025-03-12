07f3fd7f94b4421265fc91644e8382e59cd44e06
Ryan R Vargas <ryanr99@live.com>
2025-03-12 18:23:36 -0400

    still working

--- Phone.java
+++ Phone.java
@@ -4,7 +4,9 @@ public class Phone extends ElectronicsItem{
     private String model;
     private int storage;
 
-
+    public Phone(String name, String brand, String color, String carrier, String model, int storage) {
+        super(name, price, quantity,)
+    }
 
     public Phone(){
     }
