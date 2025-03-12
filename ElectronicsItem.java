604dcd3a91d8fd1d599a003639963ca5f05ed0c8
Ryan R Vargas <ryanr99@live.com>
2025-03-12 17:43:48 -0400

    Class working in the making

--- ElectronicsItem.java
+++ ElectronicsItem.java
@@ -1,2 +1,11 @@
-public class ElectronicsItem {
+public class ElectronicsItem extends Store {
+    private Boolean touchScreen;
+    private String screenType;
+    private Boolean keyboard;
+
+    public ElectronicsItem(String name, double price, int quantity, String brand) {
+        super(name, price, quantity, brand);
+
+    }
+
 }
