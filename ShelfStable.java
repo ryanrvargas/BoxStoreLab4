a4d904ec44489a07d595495d25d9354330ee0df8
Harrison Hubbard <hhubg04@gmail.com>
2025-03-13 14:11:14 -0400

    added shelfstable

---
+++ ShelfStable.java
@@ -0,0 +1,12 @@
+/**
+ * ShelfStable.java
+ * @author Harrison Hubbard
+ * Date: 3/13/2025
+ * Section: CSC-331-002
+ *
+ * Purpose: Implementing the ShelfStable  class
+ */
+
+
+public class ShelfStable {
+}
