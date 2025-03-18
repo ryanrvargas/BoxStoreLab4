5ee33006f4932e02d447a17a73571a8201211a4f
Ryan R Vargas <ryanr99@live.com>
2025-03-18 18:18:07 -0400

    DriverClass addItem Function

--- WQSVargas1Rivera2Hubbard3.java
+++ WQSVargas1Rivera2Hubbard3.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class WQSVargas1Rivera2Hubbard3 {
+    public static void addingItem(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What type of item is it?\nFood, Electronics, Clothing, or Household");
+        String type = scanner.nextLine();
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
